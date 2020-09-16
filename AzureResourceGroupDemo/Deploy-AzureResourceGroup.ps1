f599327adab047291a165fed717595cd15301c05
Pavel Chuchuva <Pavel.Chuchuva@quest.com>
2020-09-17 09:35:26 +1000

    Fix path to template file

--- AzureResourceGroupDemo/Deploy-AzureResourceGroup.ps1
+++ AzureResourceGroupDemo/Deploy-AzureResourceGroup.ps1
@@ -1,4 +1,4 @@
 Write-Output 'Hello from PowerShell!'
 
 New-AzResourceGroupDeployment -ResourceGroupName 'pavel-test' `
-  -TemplateFile 'databricks-deploy.json'
+  -TemplateFile 'AzureResourceGroupDemo\databricks-deploy.json'
